0ab1fd4f4c23fc5aa9f1b94f54f6058c70a7e0db
supersweet-dev <jim@supersweet.dev>
2022-04-27 03:12:06 -0500

    added generators with commands

---
+++ generator-txt.js
@@ -0,0 +1,23 @@
+const fs = require('fs'); //FS Dependency enables access to filesystem.
+const n = 50000;
+console.log('using random data, size %d', n);
+data = '';
+let start = 0;
+for (let i = 1; i <= n; i++) {
+  data += start;
+  start += Math.floor(Math.random() * (256 - 1) + 1);
+  data += ', ';
+  data += Math.random() * (160 - 0.1) + 0.1;
+  if (i == n) break;
+  data += '\n';
+}
+data = data + '\n]';
+const resultname = 'generated.txt';
+fs.writeFile(
+  resultname,
+  data,
+  function (err) {
+    if (err) throw err;
+    console.log(`Solution writing to ${resultname}`);
+  } //Create or overwrite result file. First line is CSV Headers.
+);
