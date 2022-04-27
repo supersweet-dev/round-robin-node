02454241e3aa2a6a76dea2b781f7a33330daa4ac
supersweet-dev <jim@supersweet.dev>
2022-04-27 00:30:26 -0500

    adding tf dep

--- round-robin.js
+++ round-robin.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
-
-const { start } = require('repl');
-
 const print = console.log;
+const tf = require('@tensorflow/tfjs');
+
+// Define a model for linear regression.
+const model = tf.sequential();
+model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+// Prepare the model for training: Specify the loss and the optimizer.
+model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+
+// Generate some synthetic data for training.
+const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+// Train the model using the data.
+model.fit(xs, ys).then(() => {
+  // Use the model to do inference on a data point the model hasn't seen before:
+  model.predict(tf.tensor2d([5], [1, 1])).print();
+});
+
 const roundRobin = () => {
   const cli = require('cli'); //CLI dependecy parses command line style inputs.
 
